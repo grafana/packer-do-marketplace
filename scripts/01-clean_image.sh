475a279920f13ec86416fac8b17679d0e082b2ac
Goutham Veeramachaneni <gouthamve@gmail.com>
2019-04-10 07:52:09 +0530

    Fix auth log handling
    Signed-off-by: Goutham Veeramachaneni <gouthamve@gmail.com>

--- scripts/01-clean_image.sh
+++ scripts/01-clean_image.sh
@@ -8,8 +8,8 @@ unset HISTFILE
 apt-get -y autoremove
 apt-get -y autoclean
 find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
-rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-???????? /var/log/*.log
+rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
 rm -rf /var/lib/cloud/instances/*
 rm -f /root/.ssh/authorized_keys /etc/ssh/*key*
 dd if=/dev/zero of=/zerofile; sync; rm /zerofile; sync
-cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp; cat /dev/null > /var/log/auth.log
